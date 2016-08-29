83651c5dae49f389423a731f76731df3e4f5e003
Dilip Yadav <dyadav@reputation.com>
2016-08-29 16:06:25 +0530

    code change for counter list

--- chamber1/src/com/github/dilipptt/programs/CounterList.java
+++ chamber1/src/com/github/dilipptt/programs/CounterList.java
@@ -1,4 +1,5 @@
 package com.github.dilipptt.programs;
+
 // Write a Counter that returns the next number. The counter has a specified range and it cycles. That is if the counter’s range is 10 to 20, and the current value of the counter is 20, when you get the next number, it will be 10.
 
 import java.util.Scanner;
@@ -15,6 +16,12 @@ class Node {
 
 public class CounterList {
 	Node head;
+
+	CounterList(int start, int end, int num) {
+		if (num < start || num > end || end < start) {
+			throw new IllegalArgumentException();
+		}
+	}
 
 	public CounterList() {
 		head = null;
@@ -45,21 +52,30 @@ public class CounterList {
 		return currentNode.next.data;
 	}
 
+	public void createList(int start, int end) {
+		Node node = null;
+		for (int i = start; i <= end; i++) {
+			node = new Node(i);
+			pushNode(node);
+		}
+	}
+
 	public static void main(String[] args) {
-		CounterList list = new CounterList();
 		@SuppressWarnings("resource")
 		Scanner scan = new Scanner(System.in);
 		System.out.print("Enter the starting number: ");
 		int start = scan.nextInt();
 		System.out.print("Enter the ending number: ");
 		int end = scan.nextInt();
+		System.out.print("Enter the number to find its next number: ");
+		int nextNum = scan.nextInt();
+		CounterList list = new CounterList(start, end, nextNum);
 		Node node = null;
 		for (int i = start; i <= end; i++) {
 			node = new Node(i);
 			list.pushNode(node);
 		}
-		System.out.print("Enter the number to find its next number: ");
-		int nextNum = list.nextNumber(scan.nextInt());
-		System.out.print("The next number in the counter is: " + nextNum);
+		System.out.print("The next number in the counter is: "
+				+ list.nextNumber(nextNum));
 	}
 }
--- chamber1/src/com/github/dilipptt/programs/CounterListTest.java
+++ chamber1/src/com/github/dilipptt/programs/CounterListTest.java
@@ -10,9 +10,49 @@ public class CounterListTest {
 
 	@Test
 	public void positiveTest() {
-		CounterList counter = new CounterList();
 		int start = 10;
 		int end = 21;
+		int nextNum = 10;
+		CounterList list = new CounterList(start, end, nextNum);
+		list.createList(start, end);
+		int actual = list.nextNumber(10);
+		assertEquals(11, actual);
 	}
 
+	@Test
+	public void positiveTestForLastNum() {
+		int start = 10;
+		int end = 21;
+		int nextNum = 21;
+		CounterList list = new CounterList(start, end, nextNum);
+		list.createList(start, end);
+		int actual = list.nextNumber(21);
+		assertEquals(10, actual);
+	}
+
+	@Test
+	public void outOfRangeTest1() {
+		int start = 10;
+		int end = 21;
+		int nextNum = 2; // less than lower bound
+		try{
+			CounterList list = new CounterList(start, end, nextNum);
+			list.createList(start, end);
+		}catch(IllegalArgumentException e){
+			fail("The number is out of range");
+		}
+	}
+	
+	@Test
+	public void outOfRangeTest2() {
+		int start = 10;
+		int end = 21;
+		int nextNum = 22; // more than lower bound
+		try{
+			CounterList list = new CounterList(start, end, nextNum);
+			list.createList(start, end);
+		}catch(IllegalArgumentException e){
+			fail("The number is out of range");
+		}
+	}
 }
