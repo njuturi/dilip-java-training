e8ce246957d6aa5634347e3787b5f0d7432c5c89
Dilip Yadav <dyadav@reputation.com>
2016-09-03 23:32:49 +0530

    fibonacci

--- chamber1/src/com/github/dilipptt/programs/Fibonacci.java
+++ chamber1/src/com/github/dilipptt/programs/Fibonacci.java
@@ -7,7 +7,7 @@ import java.util.List;
 public class Fibonacci {
 	private int number;
 	private List<Integer> fibonacciList = new ArrayList<>();
-	private ArrayList<ArrayList<Double>> listOfGoldenNumberPairs = new ArrayList<ArrayList<Double>>();
+	private ArrayList<ArrayList<Integer>> listOfGoldenNumberPairs = new ArrayList<ArrayList<Integer>>();
 
 	// private double goldenRatio = 1.618034;
 	public Fibonacci(int number) {
@@ -23,8 +23,7 @@ public class Fibonacci {
 		int temp3;
 		double ratio1;
 		double ratio2;
-		double threshold = 0.00005;
-		ArrayList<Integer> arrayList = new ArrayList<>();
+		double threshold = 0.00001;
 		fibonacciList.add(temp1);
 		if (number == 0) {
 			return fibonacciList;
@@ -35,11 +34,18 @@ public class Fibonacci {
 			ratio1 = ((double) temp2 + (double) temp1) / (temp2);
 			// ratio2 = a / b
 			ratio2 = ((double) temp2) / (double) temp1;
-			// Checking if ratio1 and ratio2 are equal
-			// I am considering the ratio are equal if the difference between
-			// them is smaller than 0.00005
+
+			// Below line will print the ratio
+			System.out.println("(" + temp2 + " + " + temp1 + ")/ " + temp2
+					+ " = " + ratio1);
+
+			// if both ratios are same the two number temp1 and temp2 form golden ratio
 			if (Math.abs(ratio2 - ratio1) <= threshold) {
-				System.out.println(temp1 + " " + temp2);
+				ArrayList<Integer> arrayList = new ArrayList<>();
+				arrayList.add(temp1);
+				arrayList.add(temp2);
+				// Adding the number pairs into list of lists
+				listOfGoldenNumberPairs.add(arrayList);
 			}
 			temp3 = temp1 + temp2;
 			temp1 = temp2;
@@ -53,8 +59,19 @@ public class Fibonacci {
 		Fibonacci fibo = new Fibonacci(20);
 		List<Integer> list = fibo.getFibonacci();
 		Iterator<Integer> it = list.iterator();
+		System.out.print("\nThe Fibonacci series is : ");
 		while (it.hasNext()) {
 			System.out.print(it.next() + " ");
 		}
+		System.out.println("\n\nThe number pairs that gives golden ratio: " + fibo.getListOfGoldenNumberPairs());
+	}
+
+	public ArrayList<ArrayList<Integer>> getListOfGoldenNumberPairs() {
+		return listOfGoldenNumberPairs;
+	}
+
+	public void setListOfGoldenNumberPairs(
+			ArrayList<ArrayList<Integer>> listOfGoldenNumberPairs) {
+		this.listOfGoldenNumberPairs = listOfGoldenNumberPairs;
 	}
 }
